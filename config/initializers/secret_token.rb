d11e235889a30f7fef26c6557d8afc0da3df5fd1
ml242 <mattlucas@gmail.com>
2014-06-26 16:08:45 -0400

    initial comit for the rbi project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RbiApp::Application.config.secret_token = 'd85ce2a2559035ab6550c6cd789d6db6e0fd1d737a2a98369ef5609bac7ab8d4b4c7a042f7267546caf56e2993fbb15e083fe2de402d25617e8025b7b5866292'
