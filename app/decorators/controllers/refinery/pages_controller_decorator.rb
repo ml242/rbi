010c152179b37d1ebd7b812c60c49f2bb4339c08
ml242 <mattlucas@gmail.com>
2014-07-15 18:33:38 -0400

    the body is working properly

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,16 +1,14 @@
 Refinery::PagesController.class_eval do
   
+    before_filter :get_index, :only => [:home]
     before_filter :find_last_blog_post, :only => [:home]
     # before_filter :find_last_blog_post
  
-    def index
-
-    end
 
     protected
     
       def get_index
-
+        
       end
 
       def find_last_blog_post
