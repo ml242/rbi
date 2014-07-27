319a62e46271f20d1a3049256b679f5598a3e3c9
ml242 <mattlucas@gmail.com>
2014-07-26 23:43:12 -0400

    dyanmically updates gallery on front page

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,13 +1,19 @@
 Refinery::PagesController.class_eval do
   
     before_filter :find_last_blog_post, :only => [:home]
-    before_filter :get_images, :only => [:show]
+    before_filter :get_last_four_galleries, :only => [:home]
+    before_filter :get_gallery_index, :only => [:gallery, :show]
  
 
     protected
     
-      def get_images
+      def get_gallery_index
+        @gallery_index = Refinery::Page.where(:parent_id => 6)
+      end
 
+      def get_last_four_galleries
+        @gallery_recent = Refinery::Page.where(:parent_id => 6)
+        @gallery_recent.find(:all, :order => "id desc", :limit => 4).reverse
       end
 
 
