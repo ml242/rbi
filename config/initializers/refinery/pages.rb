8c2f059f15fb9e75f89b7935ed210a501309abf4
ml242 <mattlucas@gmail.com>
2014-07-27 01:24:42 -0400

    working on the gallery/index

--- config/initializers/refinery/pages.rb
+++ config/initializers/refinery/pages.rb
@@ -48,7 +48,7 @@ Refinery::Pages.configure do |config|
   # Set this to true to fully expand the page hierarchy in the admin
   # config.auto_expand_admin_tree = true
 
-  # config.layout_template_whitelist = ["application"]
+  # config.layout_template_whitelist = ["application", "show", "home", "gallery"]
 
   # config.use_layout_templates = false
 
