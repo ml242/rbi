0709cd95d018f07174f3ecbaa7c4503b80ab6d3f
ml242 <mattlucas@gmail.com>
2014-08-02 15:07:55 -0400

    config.eager_load = false in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,8 @@ RbiApp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.eager_load = false
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
